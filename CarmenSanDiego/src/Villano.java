9353f8024f26bc4660711925e683e6b35b730005
cris_ <cris_@192.168.0.6>
2019-10-14 19:25:03 -0300

    Se agregan los atributos de la clase villano y algunos mensajes

--- CarmenSanDiego/src/Villano.java
+++ CarmenSanDiego/src/Villano.java
@@ -1,4 +1,22 @@
 
 public class Villano {
+	private String nombre;
+	private String sexo;
+	private List<String> hobbies;
+	private List<String> senias;
+	
+	public String obtenerNombre() {
+		return nombre;
+		}
+	
+	public void agregarHobbie(hobbie::String) {
+		if (!hobbies.contains(hobbie)) {
+			hobbies.add(hobbie);
+			}
+	}
+	
+	public String obtenerSexo() {
+		return sexo;
+		}
 
 }
