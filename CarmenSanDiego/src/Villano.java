d408920e925a48fc816b8781eb9df2be0beb17ca
cris_ <cris_@192.168.0.6>
2019-10-14 23:44:45 -0300

    Se agrego al villano los mensajes: -Agregar Señas -SetName -SetSexo
    Se modifico errores de parametros

--- CarmenSanDiego/src/Villano.java
+++ CarmenSanDiego/src/Villano.java
@@ -5,15 +5,29 @@ public class Villano {
 	private List<String> hobbies;
 	private List<String> senias;
 	
+	public void setName(String name) {
+		this.nombre=name;
+		}
+	
+	
 	public String obtenerNombre() {
 		return nombre;
 		}
 	
-	public void agregarHobbie(String::hobbie) {
+	public void agregarSenia(String senia) {
+		if (!senias.contains(senia)) {
+			hobbies.add(senia);
+			}
+	}
+		
+	public void agregarHobbie(String hobbie) {
 		if (!hobbies.contains(hobbie)) {
 			hobbies.add(hobbie);
 			}
 		
+	}
+	public void setSexo(String sexo) {
+		this.sexo=sexo;
 	}
 	
 	public String obtenerSexo() {
