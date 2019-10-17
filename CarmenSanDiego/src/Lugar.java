69227eecb428e0b59ca544fb03b505fb1352f3e5
root <root@user>
2019-10-17 20:20:49 -0300

    - Se creo clase Lugar y Caso

---
+++ CarmenSanDiego/src/Lugar.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public abstract class Lugar {
+	private String nombre;
+	
+	public Lugar(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String obtenerNombre() {
+		return nombre;
+	}
+	
+	public abstract List<String> obtenerPista(Caso caso);
+}
