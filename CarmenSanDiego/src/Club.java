bb965200e0ec5801a932a2a75d32770fc1594819
fearARGENTINA <bravogermanezequiel@gmail.com>
2019-10-20 19:41:54 -0300

    - Se creo la clase Club, la cual extiende a Lugar.

---
+++ CarmenSanDiego/src/Club.java
@@ -0,0 +1,17 @@
+package CarmenSanDiego.src;
+
+import java.util.List;
+
+public class Club extends Lugar {
+
+	public Club(String nombre) {
+		super(nombre);
+	}
+
+	/* TODO: Implementarla */
+	@Override
+	public List<String> obtenerPistas(Caso caso) {
+		return null;
+	}
+
+}
