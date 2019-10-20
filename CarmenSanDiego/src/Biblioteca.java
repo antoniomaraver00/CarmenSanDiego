a2589b7d9759a45f73b3f833642ccf5720a5bc74
fearARGENTINA <bravogermanezequiel@gmail.com>
2019-10-20 19:41:15 -0300

    - Se creo la clase Biblioteca la cual extiende a Lugar.

---
+++ CarmenSanDiego/src/Biblioteca.java
@@ -0,0 +1,17 @@
+package CarmenSanDiego.src;
+
+import java.util.List;
+
+public class Biblioteca extends Lugar {
+
+	public Biblioteca(String nombre) {
+		super(nombre);
+	}
+
+	/* TODO: Implementarla */
+	@Override
+	public List<String> obtenerPistas(Caso caso) {
+		return null;
+	}
+
+}
