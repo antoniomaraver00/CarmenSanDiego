e0b5154295829302963b5a1e3bc9957f56026f1c
fearARGENTINA <bravogermanezequiel@gmail.com>
2019-10-20 19:40:14 -0300

    - Se creo la clase Banco, la cual extende a Lugar.

---
+++ CarmenSanDiego/src/Banco.java
@@ -0,0 +1,15 @@
+package CarmenSanDiego.src;
+
+import java.util.List;
+public class Banco extends Lugar {
+	
+	public Banco(String nombre) {
+		super(nombre);
+	}
+
+	/* TODO: Implementarla */
+	@Override
+	public List<String> obtenerPistas(Caso caso) {
+		return null;
+	}
+}
