1cd0c5b4f4426a4b38b00118918b3a75671ed1ab
tomca <tomascarril>
2019-10-23 16:45:54 -0300

    Se añaden los metodos generarOrden y viajar a la clase Detective

--- CarmenSanDiego/src/Detective.java
+++ CarmenSanDiego/src/Detective.java
@@ -1,6 +1,19 @@
 package CarmenSanDiego.src;
 
 public class Detective {
-	private Villano sospechosoEnOrden;
+	private Villano sospechosoEnOrden = null;
+	private Pais paisActual;
 	
+	public void generarOrden(Villano villano)
+	{
+		sospechosoEnOrden = villano;
+	}
+	
+	public void viajar(Pais paisDestino)
+	{
+		if(paisActual.hayConexion(paisDestino))
+		{
+			paisActual = paisDestino;
+		}
+	}
 }
