10d3dfdb2cd0ca4d0518caa6a22d4151f3aaadce
fearARGENTINA <bravogermanezequiel@gmail.com>
2019-10-29 11:00:21 -0300

    Test de Detective, instancia nueva, y viaja a un Pais.

--- CarmenSanDiegoTest/DetectiveTest.java
+++ CarmenSanDiegoTest/DetectiveTest.java
@@ -1,6 +1,23 @@
 package CarmenSanDiegoTest;
 
+import org.junit.Test;
+import org.junit.Assert;
+
+import org.mockito.Mockito;
+
+import CarmenSanDiego.src.Detective;
+import CarmenSanDiego.src.Pais;
+
 public class DetectiveTest {
 	
-	
+	@Test
+	public void viajar_DetectiveRecienInstanciadoViajaAUnPais() {
+		Detective detective = new Detective();
+		
+		Pais argentina = Mockito.mock(Pais.class);
+		Mockito.when(argentina.getNombre()).thenReturn("Argentina");
+		
+		detective.viajar(argentina);
+		Assert.assertEquals(argentina, detective.obtenerPaisActual());
+	}
 }
