07d24f53e50bfd29b88842458713ecb053e2252b
tomca <tomca@DESKTOP-E4BDR6K>
2019-11-24 23:27:32 -0300

    Se agrega el metodo getVillanos a ExpedientesViewModel

--- CarmenSanDiegoModeloVistas/ExpedientesViewModel.java
+++ CarmenSanDiegoModeloVistas/ExpedientesViewModel.java
@@ -1,5 +1,13 @@
 package CarmenSanDiegoModeloVistas;
 
+import java.util.ArrayList;
+
+import CarmenSanDiego.src.Villano;
 public class ExpedientesViewModel {
 
+	private ArrayList<Villano> villanos;
+	
+	public ArrayList<Villano> getVillanos(){
+		return villanos;
+	}
 }
