311268dfaad92d183eec2ae7af003ab0c9f2919b
root <root@user>
2019-11-24 21:38:04 -0300

    - Se creo la celda, unidad minima para la creacion de JList de lugares.

---
+++ CarmenSanDiegoVistas/LugarCell.java
@@ -0,0 +1,25 @@
+package CarmenSanDiegoVistas;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import CarmenSanDiego.src.Lugar;
+
+public class LugarCell  extends JButton implements ListCellRenderer<Lugar> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4642610935328204802L;
+
+	@Override
+	public Component getListCellRendererComponent(JList<? extends Lugar> list, Lugar value, int index,
+			boolean isSelected, boolean cellHasFocus) {
+		setText(value.getNombre());
+		return this;
+	}
+
+}
